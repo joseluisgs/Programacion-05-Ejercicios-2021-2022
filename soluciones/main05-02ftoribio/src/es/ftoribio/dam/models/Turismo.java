7a4dfc70082380ecc27d78789e2778ec3fd2f1e5
Francisco Toribio Respaldo <fran_toribio@hotmail.com>
2022-03-07 22:26:32 +0100

    ejercicio 2 del tema 5 francisco toribio

---
+++ soluciones/main05-02ftoribio/src/es/ftoribio/dam/models/Turismo.java
@@ -0,0 +1,16 @@
+package es.ftoribio.dam.models;
+
+public class Turismo extends Vehiculo{
+    private int numeroPlazas;
+
+    public Turismo(String matricula, int numeroPlazas){
+        super();
+        this.setMatricula(matricula);
+        this.numeroPlazas=numeroPlazas;
+    }
+    public void tipodeUso(){
+        int uso=(int)(Math.random()*100);
+        if (uso <= 50) System.out.println("Tipo de uso Profesional");
+        else System.out.println("Tipo de uso Particular");
+    }
+}
---
+++ soluciones/main05-02ftoribio/src/es/ftoribio/dam/models/Vehiculo.java
@@ -0,0 +1,13 @@
+package es.ftoribio.dam.models;
+
+public abstract class Vehiculo {
+    private String matricula;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
